72177135aed6166e7016a2602a19066643bc3299
rkrzewski <rafal.krzewski@objectledge.org>
2011-12-01 00:53:21 +0100

    basic application using Eclipse Libra tooling

---
+++ libra/pl.caltha.osgi.libra.util/src/pl/caltha/osgi/libra/util/Utility.java
@@ -0,0 +1,7 @@
+package pl.caltha.osgi.libra.util;
+
+public class Utility {
+	public String hello() {
+		return "Hello from a class in utility bundle!";
+	}
+}
---
+++ libra/pl.caltha.osgi.libra.web/src/pl/caltha/osgi/libra/web/Servlet.java
@@ -0,0 +1,46 @@
+package pl.caltha.osgi.libra.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pl.caltha.osgi.libra.util.Utility;
+
+/**
+ * Servlet implementation class Foo
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Servlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.append("Hello from a Servlet!\n");
+		Utility util = new Utility();
+		pw.append(util.hello());
+		pw.flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
