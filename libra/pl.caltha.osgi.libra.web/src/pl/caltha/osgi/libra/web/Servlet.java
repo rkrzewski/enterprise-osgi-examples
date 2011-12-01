579ca784b6cfee911556bc9b26ebd24a286cbaa1
rkrzewski <rafal.krzewski@objectledge.org>
2011-12-01 21:57:07 +0100

    removed non-essential bits

--- libra/pl.caltha.osgi.libra.web/src/pl/caltha/osgi/libra/web/Servlet.java
+++ libra/pl.caltha.osgi.libra.web/src/pl/caltha/osgi/libra/web/Servlet.java
@@ -11,36 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import pl.caltha.osgi.libra.util.Utility;
 
-/**
- * Servlet implementation class Foo
- */
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public Servlet() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter pw = response.getWriter();
 		pw.append("Hello from a Servlet!\n");
 		Utility util = new Utility();
 		pw.append(util.hello());
-		pw.flush();
+		pw.close();
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
 }
